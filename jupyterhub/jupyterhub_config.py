66241982e1fb706c4b11f0e1537f9aa1514f7c2d
Benedikt Bäumle <benedikt.baeumle@uni-konstanz.de>
2019-11-21 11:12:39 +0100

    update jupyterhub_config to use systemuserspawner + use home directories of system users as home directory + only stop singleuser services automatically after one year of inactivity

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -15,25 +15,25 @@ import os
 c.JupyterHub.admin_access = True
 c.Spawner.default_url = '/lab'
 
+## Authentication
+
+c.JupyterHub.authenticator_class = 'jupyterhub.auth.PAMAuthenticator'
+
+
+# Make the Jupyterhub accessible
+
+c.NotebookApp.allow_origin = '*' # make it accessible from the outside world
+c.NotebookApp.ip = os.environ['IP']
+
 ## Authenticator
-from jhub_cas_authenticator.cas_auth import CASAuthenticator
-c.JupyterHub.authenticator_class = CASAuthenticator
 
-# The CAS URLs to redirect (un)authenticated users to.
-c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
-c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
-
-# The CAS endpoint for validating service tickets.
-c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
-
-# The service URL the CAS server will redirect the browser back to on successful authentication.
-c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
-
-c.Authenticator.admin_users = { 'lucadefe' }
+c.LocalAuthenticator.group_whitelist = {'jupyterhubuser'}
+c.Authenticator.admin_users = { 'sysadmin' }
 
 
 ## Docker spawner
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+#c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
@@ -41,20 +41,23 @@ c.JupyterHub.hub_ip = os.environ['HUB_IP']
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+#notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+notebook_dir = '/home/{username}'
 c.DockerSpawner.notebook_dir = notebook_dir
-c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
 # Other stuff
-c.Spawner.cpu_limit = 1
-c.Spawner.mem_limit = '10G'
+#c.Spawner.cpu_limit = 1
+c.Spawner.mem_limit = '20G'
 
+
+# Remove containers when they are stopped
+c.DockerSpawner.remove_containers = True
 
 ## Services
 c.JupyterHub.services = [
     {
         'name': 'cull_idle',
         'admin': True,
-        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
+        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=31536000'.split(),
     },
 ]
